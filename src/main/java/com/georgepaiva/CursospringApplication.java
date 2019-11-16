e31c489ee3807d24de57c674095c13e34c8b5400
George_Paiva <george.paiva15@gmail.com>
2019-11-16 15:21:56 -0300

    Criando operação de instanciação

--- src/main/java/com/georgepaiva/CursospringApplication.java
+++ src/main/java/com/georgepaiva/CursospringApplication.java
@@ -1,13 +1,35 @@
 package com.georgepaiva;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.georgepaiva.domain.Categoria;
+import com.georgepaiva.repositories.CategoriaRepository;
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements CommandLineRunner {
+
+	// Responsável por acessar os dados e salvar um objeto.
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
