c4566f2d234df21dc8d95b5cd8650b8bcd201ffe
George_Paiva <george.paiva15@gmail.com>
2019-11-19 21:54:19 -0300

    Pedido, estadoPagamento e pagamento 2

--- src/main/java/com/georgepaiva/domain/PagamentoComCartao.java
+++ src/main/java/com/georgepaiva/domain/PagamentoComCartao.java
@@ -1,7 +1,10 @@
 package com.georgepaiva.domain;
 
+import javax.persistence.Entity;
+
 import com.georgepaiva.enums.EstadoPagamento;
 
+@Entity
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
